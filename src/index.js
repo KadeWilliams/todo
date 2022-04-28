6cadbcb088a2c79090717bc2814ec596421fbbb0
KadeWilliams <kadewilliams0@gmail.com>
2022-04-28 14:59:40 -0500

    rebuilding UI file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-import ui from './modules/ui.js'
-ui();
+import { buildUI, getLocal } from './modules/ui.js'
+
+const projects = getLocal();
+
+buildUI(projects);
+
+localStorage.setItem('projects', JSON.stringify(projects))
+
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -11,37 +11,97 @@ import { Task } from "./tasks";
 when new tasks and projects are created within their own modules
 do all the necessary generation of each project (objects) and tasks (objects) in their modules
 */
-export default function ui() {
-    const projects = {};
 
-    if (Object.keys(projects.length === 0)) {
-        console.log('it is empty')
+const getLocal = () => {
+    let projects;
+    if (localStorage.getItem('projects') == "{}") {
+
+        projects = {}
+        const defaultProject = Project('Default');
+        const defaultTask = Task('Default Task');
+        defaultProject.addTask(defaultTask);
+        projects[defaultProject.getName()] = defaultProject.getTasks();
+
+    } else {
+        projects = localStorage.getItem('projects')
+        projects = JSON.parse(projects)
     }
-
-    const project = Project('Default');
-
-    const newTask = Task('title', '5/15/2022');
-
-    const task = Task('task');
-    const task1 = Task('task1');
-    const task3 = Task('task3');
-    const task4 = Task('task4');
-    const task5 = Task('task5');
-    const task6 = Task('task6');
-    const task7 = Task('task7');
-    const task8 = Task('task8');
-    const task9 = Task('task9');
-    project.addTask(task);
-    project.addTask(task1)
-    project.addTask(task3)
-    project.addTask(task4)
-    project.addTask(task5)
-    project.addTask(task6)
-    project.addTask(task7)
-    project.addTask(task8)
-    project.addTask(task9)
-
-
-    localStorage.setItem('projects', projects)
-    localStorage.getItem('projects')
+    return projects;
 }
+
+const deleteProject = (projects, project) => {
+    delete projects[project];
+    localStorage.setItem('projects', JSON.stringify(projects))
+}
+
+
+function buildUI(projects) {
+    // checks if anything is in local storage, if there's not instantiate new projects object
+    // Primary HTML element
+    const content = document.getElementById('content');
+
+    // projects pane 
+    const projectsPane = document.createElement('section');
+    projectsPane.classList.add('projects');
+
+    const newProjects = (project) => {
+        const newProj = document.createElement('div');
+        newProj.classList.add('project');
+        newProj.classList.add(`${project.replace(/\s/g, "")}`);
+        newProj.innerHTML = project;
+
+        const xBtn = document.createElement('button');
+        // xBtn.innerHTML = "🗑️";
+        xBtn.innerHTML = "X";
+        xBtn.classList.add('deleteProj');
+
+        const editBtn = document.createElement('button');
+        editBtn.innerHTML = "edit";
+        editBtn.classList.add('editProj');
+
+        const btnContainer = document.createElement('div');
+        btnContainer.classList.add('btnContainer');
+
+        btnContainer.appendChild(editBtn);
+        btnContainer.appendChild(xBtn);
+
+        newProj.append(btnContainer)
+        projectsPane.appendChild(newProj);
+    }
+    // show projects
+
+    document.addEventListener('click', function (e) {
+        if (e.target.classList.contains('deleteProj')) {
+            const projName = e.path[1].classList[1];
+            deleteProject(projects, projName)
+            projectsPane.innerHTML = ''
+            Object.keys(projects).forEach(value => newProjects(value))
+        } else if (e.target.classList.contains('project')) {
+            console.log(projects[e.path[0].classList[1]]);
+        }
+    })
+
+    const newProject = Project('newProject');
+    const newTask = Task('newTask')
+
+    newProject.addTask(newTask);
+
+    projects[newProject.getName()] = newProject.getTasks();
+
+    // gets each project 
+    Object.keys(projects).forEach(value => newProjects(value))
+
+    // add project button
+    const addProjectBtn = document.createElement('button');
+    addProjectBtn.classList.add('addProjectBtn');
+    addProjectBtn.innerHTML = '+ Add Project';
+
+    const bottomDiv = document.createElement('div');
+    bottomDiv.classList.add('bottomDiv');
+    bottomDiv.appendChild(addProjectBtn);
+
+    projectsPane.appendChild(bottomDiv);
+    content.appendChild(projectsPane)
+}
+
+export { buildUI, getLocal }
